d41a243a8843a5f22ed003b344ceda0244266367
ab-sid <absid24@gmail.com>
2022-11-24 11:03:02 +0600

    first commit

---
+++ src/Pages/Home/Category/Category.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Category = () => {
+    return (
+        <div className='mt-5'>
+            <h1 className='text-2xl text-center text-secondary'>Category</h1>
+        </div>
+    );
+};
+
+export default Category;
