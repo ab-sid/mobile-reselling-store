ced137b6d089332df68c3364566ccd56c6f381ff
ab-sid <absid24@gmail.com>
2022-11-29 12:11:18 +0600

    signup added

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
 
 const Signup = () => {
     const { register, formState: { error }, handleSubmit } = useForm();
+    const { createUser } = useContext(AuthContext);
     const handleSignup = data => {
         console.log(data);
+        createUser(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => console.log(error))
     }
     return (
         <div className='flex justify-center items-center'>
