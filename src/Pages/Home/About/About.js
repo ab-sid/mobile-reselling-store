3c78def6f7c4ee462e3ab9100100d7de1cb32efa
ab-sid <absid24@gmail.com>
2022-12-21 20:57:39 +0600

    Updated

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -4,7 +4,7 @@ import img from '../../../assets/trust.jpg';
 const About = () => {
     return (
         <div className='my-8'>
-            <h1 className='text-3xl font-bold text-center mb-8'>About Us</h1>
+            <h1 className='text-3xl font-bold text-center text-primary mb-8'>About Us</h1>
             <div className="hero bg-base-200">
                 <div className="hero-content flex-col lg:flex-row">
                     <img src={img} className="rounded-lg shadow-2xl" alt='' />
--- src/Pages/Home/Category/Category.js
+++ src/Pages/Home/Category/Category.js
@@ -10,7 +10,7 @@ const Category = () => {
     }, [])
     return (
         <div className='my-10'>
-            <h1 className='text-3xl text-center font-bold'>Category</h1>
+            <h1 className='text-3xl text-center text-primary font-bold'>Category</h1>
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10'>
                 {
                     categories.map(cat => <CategoryCard key={cat._id} cat={cat}></CategoryCard>)
